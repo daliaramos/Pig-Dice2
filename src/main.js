5c761e2c945dc95e7fc4857cc9c4a249d3be257c
dalia ramos and Masen Matthews <dalia@>
2018-03-13 11:18:06 -0700

    troubleshoot

---
+++ src/main.js
@@ -0,0 +1,58 @@
+import { Player } from './pig';
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+$(document).ready(function() {
+
+  $("form#name-input").submit(function(event) {
+    event.preventDefault();
+
+    var name1 = $("input#name1").val();
+    var name2 = $("input#name2").val();
+
+    var player1 = new Player(name1);
+    var player2 = new Player(name2);
+
+    $("#welcome-message").text("Welcome to Pig Dice, " + name1 + " and " + name2 + "!");
+    $("form#name-input").hide();
+    $("#game-interface").fadeIn();
+
+
+// player one buttons
+
+  $("button#roll1").click(function() {
+    // var rollOne =
+    player1.rollDice();
+    $("ul#player-one-roll").text(player1.roundScore);
+
+  });
+
+  $("button#hold1").click(function() {
+    player1.hold();
+    $("ul#player-one-score").text(player1.totalScore);
+    player1.win();
+  });
+
+// player two buttons
+
+  $("button#roll2").click(function() {
+    // var rollTwo =
+    player2.rollDice();
+    $("ul#player-two-roll").text(player2.roundScore);
+  });
+
+  $("button#hold2").click(function() {
+    player2.hold();
+    $("ul#player-two-score").text(player2.totalScore);
+    player2.win();
+  });
+
+    // if (diceValue === 1) {
+    // $(".well").hide();
+    //   // this.roundScore === 0;
+    // }
+
+  });
+});
---
+++ src/pig.js
@@ -0,0 +1,32 @@
+export function Player(turn) {
+  this.playerName = "";
+  this.totalScore = 0;
+  this.roundScore = [];
+  this.turn = turn;
+}
+
+Player.prototype.rollDice = function() {
+  var diceValue = Math.floor((Math.random() * 6) + 1);
+  if (diceValue === 1) {
+    alert("Next player's turn!");
+    this.roundScore = [];
+  } else {
+    this.roundScore.push(diceValue);
+    console.log(this.roundScore);
+    return diceValue;
+  }
+};
+
+Player.prototype.hold = function() {
+  var sum = this.roundScore.reduce((total, amount) => total + amount);
+  this.totalScore += sum;
+  this.roundScore = [];
+};
+
+Player.prototype.win = function() {
+  if (this.totalScore >= 100) {
+    alert("You win!");
+  } else {
+    return this.totalScore;
+  }
+};
